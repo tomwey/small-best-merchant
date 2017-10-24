839054895cef01df21c6eb211d53be00822ec5ca
tomwey <tomwey@163.com>
2017-10-24 08:58:17 +0800

    ccc

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -14,7 +14,7 @@ ActiveAdmin.register AdminUser do
     column :current_sign_in_at
     column :sign_in_count
     column "微信用户", sortable: false do |o|
-      o.wx_user_id.blank? ? image_tag(o.wx_qrcode_url, size: '60x60') : raw("#{o.wx_user.format_nickname}<br><a href=\"#{unbind_admin_admin_user_path(o)}\" class=\"btn\">解除绑定</a>")
+      o.wx_user_id.blank? ? image_tag(o.wx_qrcode_url, size: '120x120') : raw("#{o.wx_user.format_nickname}<br><a href=\"#{unbind_admin_admin_user_path(o)}\" class=\"btn\">解除绑定</a>")
     end
     column :created_at
     actions
