c71487a99ecbce0d83a7ab6d92c710d47cf3a9b3
tomwey <tomwey@163.com>
2017-11-10 12:46:42 +0800

    change send wx message func

--- app/jobs/wechat_message_send_job.rb
+++ app/jobs/wechat_message_send_job.rb
@@ -3,8 +3,14 @@ class WechatMessageSendJob < ActiveJob::Base
 
   def perform(to, tpl, url = '', data = {})
     user = User.find_by(id: to)
-    if user && user.wechat_profile
-      Wechat::Message.send(user.wechat_profile.openid, tpl, url, data)
+    if user.blank?
+      wp = WechatProfile.find_by(id: to)
+      openid = wp.openid
+    else
+      openid = user.wechat_profile.try(:openid)
+    end
+    if openid.present?
+      Wechat::Message.send(openid, tpl, url, data)
     end
   end
   
